6ca78caf3b3b581fd37c197ad338ec64e0820782
Arvid Edenheim <arved490@student.liu.se>
2020-03-03 20:02:33 +0100

    player controls

--- client/src/components/Host/Player.tsx
+++ client/src/components/Host/Player.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Image } from './'
+import React, { useState } from 'react'
 import { TrackType } from '../../utils'
+import { Play, Pause, Repeat, Shuffle, Next, Image } from './'
 import './styles/Player.scss'
+import { Previous } from './Icons'
 
 interface PlayerProps {
     track: TrackType
@@ -12,6 +13,8 @@ export const Player: React.FC<PlayerProps> = ({ track }: PlayerProps) => {
         <div className="host-content-inner">
             <div className="player">
                 <Image src={track.artwork} />
+                <h3>{track.artist}</h3>
+                <p>{track.title}</p>
                 <Controller />
             </div>
         </div>
@@ -21,34 +24,68 @@ export const Player: React.FC<PlayerProps> = ({ track }: PlayerProps) => {
 interface ControllerProps {}
 
 export const Controller: React.FC<ControllerProps> = ({}: ControllerProps) => {
+    const [isPlaying, setIsPlaying] = useState(false)
     return (
         <div className="player-controller">
             <div className="player-controller-controlls">
-                <ControllButton type="shuffle" />
-                <ControllButton type="prev" />
-                <ControllButton type="play" />
-                <ControllButton type="next" />
-                <ControllButton type="repeat" />
+                <ControllButton onClick={() => setIsPlaying(!isPlaying)} type="shuffle" />
+                <ControllButton onClick={() => setIsPlaying(!isPlaying)} type="prev" />
+                <ControllButton onClick={() => setIsPlaying(!isPlaying)} type={isPlaying ? 'pause' : 'play'} />
+                <ControllButton onClick={() => setIsPlaying(!isPlaying)} type="next" />
+                <ControllButton onClick={() => setIsPlaying(!isPlaying)} type="repeat" />
             </div>
-            <ProgressBar />
+            <ProgressBar length={100} current={45} />
         </div>
     )
 }
 
 interface ControllButtonProps {
-    type: 'shuffle' | 'prev' | 'play' | 'next' | 'repeat'
+    type: 'shuffle' | 'prev' | 'play' | 'pause' | 'next' | 'repeat'
+    onClick: () => void
 }
 
-export const ControllButton: React.FC<ControllButtonProps> = ({ type }: ControllButtonProps) => {
-    return <button className="player-controll-button">{type}</button>
+export const ControllButton: React.FC<ControllButtonProps> = ({ type, onClick }: ControllButtonProps) => {
+    const isCircled = type == 'play' || type == 'pause'
+    const renderIcon = () => {
+        switch (type) {
+            case 'next':
+                return <Next />
+            case 'play':
+                return <Play />
+            case 'prev':
+                return <Previous />
+            case 'pause':
+                return <Pause />
+            case 'shuffle':
+                return <Shuffle />
+            case 'repeat':
+                return <Repeat />
+        }
+    }
+    return (
+        <button onClick={onClick} className={'player-controll-button ' + type + (isCircled ? ' circle' : '')}>
+            {renderIcon()}
+        </button>
+    )
 }
 
-interface ProgressBarProps {}
+interface ProgressBarProps {
+    length: number
+    current: number
+}
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({}: ProgressBarProps) => {
+export const ProgressBar: React.FC<ProgressBarProps> = ({ length, current }: ProgressBarProps) => {
+    const styles = {
+        transform: `scaleX(${current / length})`,
+    }
+
     return (
         <div className="progress-bar-container">
-            <div className="progress-bar"></div>
+            <div>{current}</div>
+            <div className="progress-bar">
+                <div style={styles}></div>
+            </div>
+            <div>{length}</div>
         </div>
     )
 }

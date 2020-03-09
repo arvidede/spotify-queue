e46014dad53363469d196ee72a9e8ba7c7b35e23
Arvid Edenheim <arved490@student.liu.se>
2020-03-09 23:24:21 +0100

    env files

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,6 +1,7 @@
 /* Spotify */
 export const CLIENT_TOKEN = process.env.CLIENT_TOKEN
 export const CLIENT_TOKEN_SECRET = process.env.CLIENT_TOKEN_SECRET
+export const USER_AUTH_URL = 'https://accounts.spotify.com/authorize'
 export const TOKEN_URL = 'https://accounts.spotify.com/api/token'
 export const BASE_URL = 'https://api.spotify.com'
 export const SEARCH_URL = BASE_URL + '/v1/search'
@@ -8,6 +9,20 @@ export const SEARCH_URL = BASE_URL + '/v1/search'
 export const TOKEN_BASE_64 = new Buffer(
     `${CLIENT_TOKEN}:${CLIENT_TOKEN_SECRET}`,
 ).toString('base64')
+
+export const APP_TOKEN_TEMPLATE = {
+    access_token: '',
+    token_type: '',
+    expires_in: 0,
+}
+
+export const USER_TOKEN_TEMPLATE = {
+    access_token: '',
+    token_type: '',
+    scope: '',
+    expires_in: 0,
+    refresh_token: '',
+}
 
 /* Firebase */
 export const DATABASE_URL = 'https://queue-1337.firebaseio.com'
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: __dirname + '/../.env' })
 const Server = require('./server')
 
 const server = new Server()

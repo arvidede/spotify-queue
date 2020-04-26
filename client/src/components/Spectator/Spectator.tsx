e272c03754747f3926e5e0524ec79262fc62894f
Arvid Edenheim <arved490@student.liu.se>
2020-04-26 18:19:12 +0200

    queue hook

--- client/src/components/Spectator/Spectator.tsx
+++ client/src/components/Spectator/Spectator.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Header } from '../Common/'
 import { PulseLoader as Spinner } from 'react-spinners'
 import { TrackList, Search, SearchResults } from './'
 import { RouteComponentProps } from 'react-router'
-import { TrackType, useSubscribers, useAPI } from '../../utils'
+import { TrackType, useSubscribers, useAPI, useSearch, useQueue } from '../../utils'
 import './styles/Spectator.scss'
-import { useSearch } from '../../utils/helpers'
 
 interface MatchParams {
     id: string
@@ -15,7 +14,7 @@ interface Props extends RouteComponentProps<MatchParams> {}
 
 export const Spectator: React.FC<Props> = React.memo(({ match }: Props) => {
     const { searching, setSearching, setSearchInput, search } = useSearch()
-    const [queue, setQueue] = useState<TrackType[]>([])
+    const queue = useQueue()
 
     const subscribers = useSubscribers(match.params.id)
     const api = useAPI()
@@ -30,14 +29,6 @@ export const Spectator: React.FC<Props> = React.memo(({ match }: Props) => {
         if (!searching) setSearching(true)
     }
 
-    useEffect(() => {
-        api.doGetQueue().then(setQueue)
-    }, [api])
-
-    const handleAddTrackToQueue = (track: string) => {
-        api.doAddTrackToQueue(track)
-    }
-
     return (
         <div className="spectator">
             <Header color="green" size="s" numSubscribers={subscribers} />
@@ -46,12 +37,12 @@ export const Spectator: React.FC<Props> = React.memo(({ match }: Props) => {
                 search.loading ? (
                     <Spinner css={'margin-top: 10vh;'} size={10} color={'white'} />
                 ) : search.result && search.result.length > 0 ? (
-                    <SearchResults onAddTrack={handleAddTrackToQueue} tracks={search.result} />
+                    <SearchResults onAddTrack={queue.addToQueue} tracks={search.result} />
                 ) : (
                     <p>No results</p>
                 )
             ) : (
-                <TrackList onVote={(s: string) => console.log(s)} tracks={queue} />
+                <TrackList onVote={(s: string) => console.log(s)} tracks={queue.tracks} />
             )}
         </div>
     )

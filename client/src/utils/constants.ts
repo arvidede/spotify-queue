88c63c3cb8696f1129f8a4300a9a5eee7a935ff2
Arvid Edenheim <arved490@student.liu.se>
2020-05-03 00:37:26 +0200

    spotify web api urls

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -11,6 +11,13 @@ export const GET_QUEUE_URL = BASE_URL + 'get-queue'
 export const ADD_TRACK_TO_QUEUE_URL = BASE_URL + 'add-to-queue'
 export const REMOVE_TRACK_FROM_QUEUE_URL = BASE_URL + 'remove-from-queue'
 
+/* Spotify API */
+export const SPOTIFY_PLAYER_BASE_URL = 'https://api.spotify.com/v1/me/player'
+export const SPOTIFY_PLAYER_PLAY_URL = SPOTIFY_PLAYER_BASE_URL + '/play'
+export const SPOTIFY_PLAYER_PAUSE_URL = SPOTIFY_PLAYER_BASE_URL + '/pause'
+export const SPOTIFY_PLAYER_NEXT_TRACK_URL = SPOTIFY_PLAYER_BASE_URL + '/next'
+export const SPOTIFY_PLAYER_PREVIOUS_TRACK_URL = SPOTIFY_PLAYER_BASE_URL + '/previous'
+
 /* Localstorage */
 export const SPOTIFY_USER_TOKEN = 'spotifyUserToken'
 export const VOTED_TRACKS = 'votes'

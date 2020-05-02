23b5bd841cd72549b0751bf3bf017891b3f991d9
Arvid Edenheim <arved490@student.liu.se>
2020-05-03 00:40:36 +0200

    standardized token formating

--- client/src/components/Start/Redirect.tsx
+++ client/src/components/Start/Redirect.tsx
@@ -10,11 +10,7 @@ export const Redirect: React.FC<RedirectProps> = props => {
     useEffect(() => {
         const query = new URLSearchParams(props.location.search)
         const code = query.get('code')
-        api.doFetchUserToken(code).then(token => {
-            token.expires_on = Date.now() + token.expires_in * 1000
-            localStorage.setItem(SPOTIFY_USER_TOKEN, JSON.stringify(token))
-            window.close()
-        })
-    })
+        api.doFetchUserToken(code).then(() => window.close)
+    }, [])
     return <div></div>
 }

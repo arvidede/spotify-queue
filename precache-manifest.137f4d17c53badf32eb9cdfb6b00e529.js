00bf417fee7e598ec563f74fdc743e97e1f05622
Arvid Edenheim <arved490@student.liu.se>
2020-05-09 23:25:40 +0200

    Updates

---
+++ precache-manifest.137f4d17c53badf32eb9cdfb6b00e529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f854e33afd2be35cf3dcac0ea63a276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc0be6a2a1eae8a4d8d",
+    "url": "/static/css/main.c841942e.chunk.css"
+  },
+  {
+    "revision": "cf984f9913ca37559aaa",
+    "url": "/static/js/2.3c7757dc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3c7757dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfc0be6a2a1eae8a4d8d",
+    "url": "/static/js/main.a23854bf.chunk.js"
+  },
+  {
+    "revision": "533b69a3f54191223424",
+    "url": "/static/js/runtime-main.e90d9a9e.js"
+  },
+  {
+    "revision": "a1286edb2706579e2113405125390bdb",
+    "url": "/static/media/placeholder.a1286edb.png"
+  }
+]);

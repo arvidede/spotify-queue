f75141875cd5f0ff8f2d28e873f28410f553ce02
Arvid Edenheim <arvidede@icloud.com>
2020-05-28 23:23:50 +0200

    allow cors for dev

--- server/src/server.js
+++ server/src/server.js
@@ -99,7 +99,7 @@ class Server {
         this.app.use(this.session)
 
         const corsOptions = {
-            origin: ['https://queue.rocks', 'https://accounts.spotify.com'],
+            origin: ['https://queue.rocks', 'https://accounts.spotify.com', 'http://localhost:3000'],
             methods: 'GET,POST,PUT,DELETE',
             allowedHeaders: [
                 'Origin',

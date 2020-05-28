6fd7340287b00b07c5d61d5a3bc365bb4c2e942d
Arvid Edenheim <arvidede@icloud.com>
2020-05-28 23:43:37 +0200

    env vars for production

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,6 @@
 /* URLs */
-export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8081/'
-export const SOCKET_URL = process.env.REACT_APP_WS_URL || 'ws://localhost:8081'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : 'http://localhost:8081/'
+export const SOCKET_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_WS_URL : 'ws://localhost:8081'
 export const HOST_URL = BASE_URL + 'host'
 export const SEARCH_URL = BASE_URL + 'search'
 export const VALIDATE_ROOM_URL = BASE_URL + 'validate'
